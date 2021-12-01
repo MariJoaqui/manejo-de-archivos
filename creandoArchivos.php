3ef8a5527f5dde5f7e46ac0314c5f225a0ceb957
MariJoaqui <84864759+MariJoaqui@users.noreply.github.com>
2021-12-01 14:08:44 -0400

    santoodio

--- creandoArchivos.php
+++ creandoArchivos.php
@@ -4,9 +4,9 @@
 $nombre = $_REQUEST['nombreArchivo'];
 $contenido = $_REQUEST['contenidoArchivo'];
 
-$ruta = $_SERVER['DOCUMENT_ROOT'] . '/ManejoDeArchivos/ArchivosTxt' . "/$nombre";
+$ruta = $_SERVER['DOCUMENT_ROOT'] . '/ManejoDeArchivos/ArchivosTxt';
 
-$file = fopen($ruta . '.txt', 'a+') or die ("Error al crear archivo"); //Se crea el archivo txt y se establece su ruta
+$file = fopen($ruta . "/$nombre" . '.txt', 'a+') or die ("Error al crear archivo"); //Se crea el archivo txt y se establece su ruta
 
 fwrite($file, $contenido); //Se añade al archivo
 
